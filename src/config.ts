dcd91a7e0e959431aa6a0f38315a4b24cca47692
Munavir Ch <munavirch@Munavirs-MacBook-Air.local>
2025-08-21 20:38:23 +0530

    fixing site domain

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  website: "https://munavirch.github.io/", // replace this with your deployed domain
   author: "Munavir Chavody",
   profile: "https://satnaing.dev/",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
